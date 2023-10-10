b957af6d2a74ca7f5c112aec782e1c14b5861cce
GrosJambon1243 <kevindurocher@live.ca>
2023-10-10 09:52:25 -0400

    HpBar Finish

--- Assets/Script/healthBar.cs
+++ Assets/Script/healthBar.cs
@@ -8,16 +8,20 @@ public class healthBar : MonoBehaviour
 {
    public Slider slider;
    public TMP_Text hpTexte;
+   public Gradient gradient;
+   public Image fill;
 
 
    public void SetMaxHealth(int maxHp)
    {
       slider.maxValue = maxHp;
-     
+      fill.color = gradient.Evaluate(1f);
+
    }
    public void SetHealth(int health, int maxHp)
    {
       slider.value = health;
       hpTexte.text = health + " / " + maxHp;
+      fill.color = gradient.Evaluate(slider.normalizedValue);
    }
 }
