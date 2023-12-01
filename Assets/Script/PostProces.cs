d64e31bef229da6cf5ad9dd6724e643f0e118829
felixsoum <felix@felixsoum.com>
2023-12-01 09:28:05 -0500

    Ajout changement graduel pour le CA

--- Assets/Script/PostProces.cs
+++ Assets/Script/PostProces.cs
@@ -10,33 +10,36 @@ public class PostProces : MonoBehaviour
     private Vignette _vignette;
     private ChromaticAberration _chromatic;
     [SerializeField] private PlayerMovement _player;
-    
+
+    float invincibleTimer;
 
     private void Awake()
     {
         _volume.profile.TryGet(out _chromatic);
         _volume.profile.TryGet(out _vignette);
-        
-    }
 
-  
+    }
 
     private void Update()
     {
-        switch (_player.IsInvincible)
+        if (_player.IsInvincible)
         {
-            case true:
-                _chromatic.intensity.Override(1f);
-                break;
-            case false:
-                _chromatic.intensity.Override(0f);
-                break;
+            invincibleTimer = 1f;
         }
+        else
+        {
+            invincibleTimer -= Time.deltaTime * 2f;
+            if (invincibleTimer < 0)
+            {
+                invincibleTimer = 0;
+            }
+        }
+        _chromatic.intensity.Override(invincibleTimer);
 
         if (_player.CurrentHp <= 100)
         {
             _vignette.active = true;
-            float x = Mathf.Sin((Time.time * 5) + 2) /4f;
+            float x = Mathf.Sin((Time.time * 5) + 2) / 4f;
             _vignette.intensity.Override(x);
         }
         else
